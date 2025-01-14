787e8688f446ddb47ea21401bd8e709e3545ebb9
davidorser <davidorser@gmail.com>
2025-01-14 17:56:12 -0600

    trying to merge main...failing horribly

--- src/robot.py
+++ src/robot.py
@@ -8,13 +8,13 @@ from simHAL import RobotSimHAL
 from timing import TimeData
 from wpimath.geometry import Pose2d, Rotation2d, Translation2d
 from wpimath.kinematics import ChassisSpeeds, SwerveModulePosition
-from robotHAL import RobotHAL
+
 
 class RobotInputs:
     def __init__(self) -> None:
-        
-
-        pass
+        self.driveCtrlr = wpilib.XboxController(0)
+        self.mechCtrlr = wpilib.XboxController(1)
+        self.buttonPanel = wpilib.Joystick(4)
 
     def update(self) -> None:
         pass
@@ -39,7 +39,9 @@ class Robot(wpilib.TimedRobot):
 
         self.table = NetworkTableInstance.getDefault().getTable("telemetry")
 
-        self.input = RobotInputs()
+        self.driveCtrlr = wpilib.XboxController(0)
+        self.mechCtrlr = wpilib.XboxController(1)
+        self.buttonPanel = wpilib.Joystick(4)
 
     def robotPeriodic(self) -> None:
         self.time = TimeData(self.time)
@@ -55,7 +57,10 @@ class Robot(wpilib.TimedRobot):
     def teleopPeriodic(self) -> None:
         self.hal.driveVolts = self.driveCtrlr.getLeftY()
         self.hal.stopMotors()
+
+        self.hal.publish(self.table)
         self.hardware.update(self.hal, self.time)
+
     def autonomousPeriodic(self) -> None:
         self.hal.stopMotors()
 
