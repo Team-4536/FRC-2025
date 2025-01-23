74c69ebdb18dfdd6246eff860bf1a1dfb032fe97
Michael-Foley <mfoley001@stpaul.k12.mn.us>
2025-01-22 19:58:51 -0600

    working PID

--- src/robotHAL.py
+++ src/robotHAL.py
@@ -38,12 +38,43 @@ class RobotHAL:
 
         self.table = NetworkTableInstance.getDefault().getTable("telemetry")
 
+        # max velocity (RPM), max acceleration (RPM / s), allowed closed loop error, minimum output velocity,
         self.desiredSpeed = 0
         self.table.putNumber("desired speed", self.desiredSpeed)
-        self.MotorP = 0
+        self.MotorP = 0.00019
         self.table.putNumber("P", self.MotorP)
+        self.MotorFF = 0.00002
+        self.table.putNumber("FF", self.MotorFF)
+
+        self.maxVelocity = 2000
+        self.maxAcceleration = 4000
+        self.closedLoopError = 1
+
+        customConfig = rev.SparkBaseConfig()
+        customConfig.closedLoop.pidf(
+            self.MotorP, 0, 0, self.MotorFF, rev.ClosedLoopSlot.kSlot0
+        ).setFeedbackSensor(
+            rev.ClosedLoopConfig.FeedbackSensor.kPrimaryEncoder
+        ).outputRange(
+            -1.0, 1.0, rev.ClosedLoopSlot.kSlot0
+        )
+
+        customConfig.closedLoop.maxMotion.maxVelocity(
+            self.maxVelocity, rev.ClosedLoopSlot.kSlot0
+        ).maxAcceleration(
+            self.maxAcceleration, rev.ClosedLoopSlot.kSlot0
+        ).allowedClosedLoopError(
+            1
+        )
+
+        error = self.driveMotorFL.configure(
+            customConfig,
+            rev.SparkBase.ResetMode.kNoResetSafeParameters,
+            rev.SparkBase.PersistMode.kNoPersistParameters,
+        )
 
         self.driveFLClosedLoopController = self.driveMotorFL.getClosedLoopController()
+        self.driveFLEncoder = self.driveMotorFL.getEncoder()
 
     # angle expected in CCW rads
     def resetGyroToAngle(self, ang: float) -> None:
@@ -59,10 +90,9 @@ class RobotHAL:
         # self.driveMotorFL.setVoltage(buf.driveVolts*1.6)
         # self.table.putNumber('hal drive volts', buf.driveVolts)
 
-        driveFLEncoder = self.driveMotorFL.getEncoder()
         driveFLPercentVoltage = self.driveMotorFL.getAppliedOutput()
-        driveFLSpeedFeedback = driveFLEncoder.getVelocity()
-        driveFLPosition = driveFLEncoder.getPosition()
+        driveFLSpeedFeedback = self.driveFLEncoder.getVelocity()
+        driveFLPosition = self.driveFLEncoder.getPosition()
         driveFLVoltage = (
             self.driveMotorFL.getAppliedOutput() * self.driveMotorFL.getBusVoltage()
         )
@@ -71,25 +101,46 @@ class RobotHAL:
         self.table.putNumber("DriveMotorFL Position", driveFLPosition)
         self.table.putNumber("DriveMotorFL percent voltage", driveFLPercentVoltage)
 
-        self.MotorP = self.table.getNumber("P", self.MotorP)
+        # self.MotorP = self.table.getNumber("P", self.MotorP)
+        # self.MotorFF = self.table.getNumber("FF", self.MotorFF)
 
         driveUniversalConfig = rev.SparkBaseConfig()
 
-        driveUniversalConfig.closedLoop.pidf(
-            self.MotorP, 0, 0, 0, rev.ClosedLoopSlot.kSlot0
-        )
-        self.driveMotorFL.configure(
-            driveUniversalConfig,
-            rev.SparkBase.ResetMode.kNoResetSafeParameters,
-            rev.SparkBase.PersistMode.kNoPersistParameters,
-        )
+        if (
+            abs(self.table.getNumber("P", self.MotorP) - self.MotorP) < 1e-6
+            or abs(self.table.getNumber("FF", self.MotorFF) - self.MotorFF) < 1e-6
+        ):
+            self.MotorP = self.table.getNumber("P", self.MotorP)
+            self.MotorFF = self.table.getNumber("FF", self.MotorFF)
+
+            driveUniversalConfig.closedLoop.pidf(
+                self.MotorP, 0, 0, self.MotorFF, rev.ClosedLoopSlot.kSlot0
+            ).setFeedbackSensor(
+                rev.ClosedLoopConfig.FeedbackSensor.kPrimaryEncoder
+            ).outputRange(
+                -1.0, 1.0, rev.ClosedLoopSlot.kSlot0
+            )
+
+            driveUniversalConfig.encoder.positionConversionFactor(
+                1
+            ).velocityConversionFactor(1)
+
+            error = self.driveMotorFL.configure(
+                driveUniversalConfig,
+                rev.SparkBase.ResetMode.kNoResetSafeParameters,
+                rev.SparkBase.PersistMode.kNoPersistParameters,
+            )
+
+            self.table.putNumber("Config err", error.value)
 
         self.desiredSpeed = self.table.getNumber("desired speed", self.desiredSpeed)
         self.driveFLClosedLoopController.setReference(
             self.desiredSpeed,
-            rev.SparkBase.ControlType.kVelocity,
+            rev.SparkBase.ControlType.kMAXMotionVelocityControl,
             rev.ClosedLoopSlot.kSlot0,
         )
+
+        # self.table.putNumber("read p value", driveUniversalConfig.closedLoop.)
 
     class PIDs:
 
