1fae2cd2ad5ec5efafc0b6c2292cab2adf41c555
CallmeElodie <lfam5718@gmail.com>
2025-01-23 17:28:50 -0600

    messing with PID class

--- src/robotHAL.py
+++ src/robotHAL.py
@@ -38,13 +38,18 @@ class RobotHAL:
         self.driveMotorBL = rev.SparkMax(6, rev.SparkLowLevel.MotorType.kBrushless)
         self.driveMotorBR = rev.SparkMax(8, rev.SparkLowLevel.MotorType.kBrushless)
 
-        self.table = NetworkTableInstance.getDefault().getTable("telemetry")
-        
-        self.driveMotorUniversalP = 0
-        self.table.putNumber("DriveMotorP", self.driveMotorFLP)
-        self.driveMotorUniversalDesiredSpeed = 0
-        self.table.putNumber("DriveMotorFLDesiredSpeed", self.driveMotorUniversalDesiredSpeed)
+        self.driveMotorFLPID = revPID(self.driveMotorFL)
+        self.driveMotorFLPID = revPID(self.driveMotorFR)
+        self.driveMotorFLPID = revPID(self.driveMotorBL)
+        self.driveMotorFLPID = revPID(self.driveMotorBL)
 
+
+
+        self.driveMotorFLClosedLoopControl = self.driveMotorFL.getClosedLoopController()
+        self.driveMotorFRClosedLoopControl = self.driveMotorFR.getClosedLoopController()
+        self.driveMotorBLClosedLoopControl = self.driveMotorFR.getClosedLoopController()
+        self.driveMotorBRClosedLoopControl = self.driveMotorFR.getClosedLoopController()
+        
         self.driveMotorFLClosedLoopControl = self.driveMotorFL.getClosedLoopController()
         self.driveMotorFRClosedLoopControl = self.driveMotorFR.getClosedLoopController()
         self.driveMotorBLClosedLoopControl = self.driveMotorFR.getClosedLoopController()
@@ -60,80 +65,104 @@ class RobotHAL:
         prev = self.prev
         self.prev = copy.deepcopy(buf)
 
-        self.driveMotorFL.setVoltage(buf.driveVolts*1.6)
-        self.table.putNumber('hal drive volts', buf.driveVolts)
 
-        self.driveMotorUniversalP = self.table.getNumber("DriveMotorP", self.driveMotorUniversalP)
-        self.driveMotorUniversalDesiredSpeed = self.table.getNumber("DriveMotor UniversalDesiredSpeed", self.driveMotorUniversalDesiredSpeed)
-        driveMotorUniversalConfig = rev.SparkBaseConfig()
-        driveMotorUniversalConfig.closedLoop.pidf(self.driveMotorUniversalP, 0, 0, 1/473, rev.ClosedLoopSlot.kSlot0)
+        # self.driveMotorFL.setVoltage(buf.driveVolts*1.6)
+        # self.table.putNumber('hal drive volts', buf.driveVolts)  
 
-        driveMotorFLEncoder = self.driveMotorFL.getEncoder()
-        driveMotorFLPercentVoltage = self.driveMotorFL.getAppliedOutput()
-        driveMotorFLSpeedFeedback = driveMotorFLEncoder.getVelocity()
-        driveMotorFLPosition = driveMotorFLEncoder.getPosition()
-        driveMotorFLVoltage = self.driveMotorFL.getAppliedOutput()*self.driveMotorFL.getBusVoltage()
-        self.table.putNumber("DriveMotorFL Speed", driveMotorFLSpeedFeedback)
-        self.table.putNumber("DriveMotorFL Position", driveMotorFLPosition)
-        self.table.putNumber("DriveMotorFL percentVoltage", driveMotorFLPercentVoltage)
-        self.table.putNumber("DriveMotorFL Voltage", driveMotorFLVoltage)
+        # driveMotorFREncoder = self.driveMotorFR.getEncoder()
+        # driveMotorFRPercentVoltage = self.driveMotorFR.getAppliedOutput()
+        # driveMotorFRSpeedFeedback = driveMotorFREncoder.getVelocity()
+        # driveMotorFRPosition = driveMotorFREncoder.getPosition()
+        # driveMotorFRVoltage = self.driveMotorFR.getAppliedOutput()*self.driveMotorFR.getBusVoltage()
+        # self.table.putNumber("DriveMotorFL Speed", driveMotorFRSpeedFeedback)
+        # self.table.putNumber("DriveMotorFL Position", driveMotorFRPosition)
+        # self.table.putNumber("DriveMotorFL percentVoltage", driveMotorFRPercentVoltage)
+        # self.table.putNumber("DriveMotorFL Voltage", driveMotorFRVoltage)
 
-        driveMotorFREncoder = self.driveMotorFR.getEncoder()
-        driveMotorFRPercentVoltage = self.driveMotorFR.getAppliedOutput()
-        driveMotorFRSpeedFeedback = driveMotorFREncoder.getVelocity()
-        driveMotorFRPosition = driveMotorFREncoder.getPosition()
-        driveMotorFRVoltage = self.driveMotorFR.getAppliedOutput()*self.driveMotorFR.getBusVoltage()
-        self.table.putNumber("DriveMotorFL Speed", driveMotorFRSpeedFeedback)
-        self.table.putNumber("DriveMotorFL Position", driveMotorFRPosition)
-        self.table.putNumber("DriveMotorFL percentVoltage", driveMotorFRPercentVoltage)
-        self.table.putNumber("DriveMotorFL Voltage", driveMotorFRVoltage)
+        # driveMotorBLEncoder = self.driveMotorBL.getEncoder()
+        # driveMotorBLPercentVoltage = self.driveMotorBL.getAppliedOutput()
+        # driveMotorBLSpeedFeedback = driveMotorBLEncoder.getVelocity()
+        # driveMotorBLPosition = driveMotorBLEncoder.getPosition()
+        # driveMotorBLVoltage = self.driveMotorBL.getAppliedOutput()*self.driveMotorBL.getBusVoltage()
+        # self.table.putNumber("DriveMotorFL Speed", driveMotorBLSpeedFeedback)
+        # self.table.putNumber("DriveMotorFL Position", driveMotorBLPosition)
+        # self.table.putNumber("DriveMotorFL percentVoltage", driveMotorBLPercentVoltage)
+        # self.table.putNumber("DriveMotorFL Voltage", driveMotorBLVoltage)
 
-        driveMotorBLEncoder = self.driveMotorBL.getEncoder()
-        driveMotorBLPercentVoltage = self.driveMotorBL.getAppliedOutput()
-        driveMotorBLSpeedFeedback = driveMotorBLEncoder.getVelocity()
-        driveMotorBLPosition = driveMotorBLEncoder.getPosition()
-        driveMotorBLVoltage = self.driveMotorBL.getAppliedOutput()*self.driveMotorBL.getBusVoltage()
-        self.table.putNumber("DriveMotorFL Speed", driveMotorBLSpeedFeedback)
-        self.table.putNumber("DriveMotorFL Position", driveMotorBLPosition)
-        self.table.putNumber("DriveMotorFL percentVoltage", driveMotorBLPercentVoltage)
-        self.table.putNumber("DriveMotorFL Voltage", driveMotorBLVoltage)
-
-        driveMotorBREncoder = self.driveMotorBR.getEncoder()
-        driveMotorBRPercentVoltage = self.driveMotorBR.getAppliedOutput()
-        driveMotorBRSpeedFeedback = driveMotorBREncoder.getVelocity()
-        driveMotorBRPosition = driveMotorBREncoder.getPosition()
-        driveMotorBRVoltage = self.driveMotorBR.getAppliedOutput()*self.driveMotorBR.getBusVoltage()
-        self.table.putNumber("DriveMotorFL Speed", driveMotorBRSpeedFeedback)
-        self.table.putNumber("DriveMotorFL Position", driveMotorBRPosition)
-        self.table.putNumber("DriveMotorFL percentVoltage", driveMotorBRPercentVoltage)
-        self.table.putNumber("DriveMotorFL Voltage", driveMotorBRVoltage)
+        # driveMotorBREncoder = self.driveMotorBR.getEncoder()
+        # driveMotorBRPercentVoltage = self.driveMotorBR.getAppliedOutput()
+        # driveMotorBRSpeedFeedback = driveMotorBREncoder.getVelocity()
+        # driveMotorBRPosition = driveMotorBREncoder.getPosition()
+        # driveMotorBRVoltage = self.driveMotorBR.getAppliedOutput()*self.driveMotorBR.getBusVoltage()
+        # self.table.putNumber("DriveMotorFL Speed", driveMotorBRSpeedFeedback)
+        # self.table.putNumber("DriveMotorFL Position", driveMotorBRPosition)
+        # self.table.putNumber("DriveMotorFL percentVoltage", driveMotorBRPercentVoltage)
+        # self.table.putNumber("DriveMotorFL Voltage", driveMotorBRVoltage)
         
 
         #driveMotorUniversalConfig.closedLoop.P(self.driveMotorFLP)
-        self.driveMotorFL.configure(
-            driveMotorUniversalConfig, 
-            rev.SparkBase.ResetMode.kNoResetSafeParameters, 
-            rev.SparkBase.PersistMode.kNoPersistParameters)
-        self.driveMotorFR.configure(
-            driveMotorUniversalConfig, 
-            rev.SparkBase.ResetMode.kNoResetSafeParameters, 
-            rev.SparkBase.PersistMode.kNoPersistParameters)
-        self.driveMotorBL.configure(
-            driveMotorUniversalConfig, 
-            rev.SparkBase.ResetMode.kNoResetSafeParameters, 
-            rev.SparkBase.PersistMode.kNoPersistParameters)
-        self.driveMotorBR.configure(
-            driveMotorUniversalConfig, 
+        
+    
+
+class revPID:
+    def __init__(self, driveMotorIn)->None:
+
+        self.driveMotor = driveMotorIn
+        self.table = NetworkTableInstance.getDefault().getTable("telemetry")
+
+        self.driveMotorUniversalP = 0
+        self.table.putNumber("DriveMotorP", self.driveMotorFLP)
+        self.driveMotorUniversalDesiredSpeed = 0
+        self.table.putNumber("DriveMotorFLDesiredSpeed", self.driveMotorUniversalDesiredSpeed)
+        
+
+        self.driveMotorEncoder = self.driveMotor.getEncoder()
+        self.driveMotorPercentVoltage = self.driveMotor.getAppliedOutput()
+        self.driveMotorSpeedFeedback = self.driveMotor.getVelocity()
+        self.driveMotorPosition = self.driveMotor.getPosition()
+        self.driveMotorVoltage = self.driveMotor.getAppliedOutput()*self.driveMotor.getBusVoltage()
+
+        self.table.putNumber("DriveMotorFL Speed", self.driveMotorSpeedFeedback)
+        self.table.putNumber("DriveMotorFL Position", self.driveMotorPosition)
+        self.table.putNumber("DriveMotorFL percentVoltage", self.driveMotorPercentVoltage)
+        self.table.putNumber("DriveMotorFL Voltage", self.driveMotorVoltage)
+
+        self.driveMotorClosedLoopControl = self.driveMotor.getClosedLoopController()
+        self.driveMotorUniversalConfig = rev.SparkBaseConfig()
+        self.driveMotorUniversalConfig.closedLoop.pidf(self.driveMotorUniversalP, 0, 0, 1/473, rev.ClosedLoopSlot.kSlot0)
+
+        
+
+    def periodic(self)->None:
+        pass
+
+    def update(self)->None:
+
+        self.driveMotorUniversalP = self.table.getNumber("DriveMotorP", self.driveMotorUniversalP)
+        self.driveMotorUniversalDesiredSpeed = self.table.getNumber("DriveMotor UniversalDesiredSpeed", self.driveMotorUniversalDesiredSpeed)
+        
+        self.driveMotor.configure(
+            self.driveMotorUniversalConfig, 
             rev.SparkBase.ResetMode.kNoResetSafeParameters, 
             rev.SparkBase.PersistMode.kNoPersistParameters)
         
-        self.driveMotorFLClosedLoopControl.setReference(self.desiredSpeed, rev.SparkBase.ControlType.kVelocity)
-        self.driveMotorFRClosedLoopControl.setReference(self.desiredSpeed, rev.SparkBase.ControlType.kVelocity)
-        self.driveMotorBLClosedLoopControl.setReference(self.desiredSpeed, rev.SparkBase.ControlType.kVelocity)
-        self.driveMotorBRClosedLoopControl.setReference(self.desiredSpeed, rev.SparkBase.ControlType.kVelocity)
-
+        
+        self.driveMotorClosedLoopControl.setReference(self.desiredSpeed, rev.SparkBase.ControlType.kVelocity)
+       
         self.table.putNumber("desiredSpeedConfirm", self.desiredSpeed)
-    
+
+        self.driveMotorEncoder = self.driveMotor.getEncoder()
+        self.driveMotorPercentVoltage = self.driveMotor.getAppliedOutput()
+        self.driveMotorSpeedFeedback = self.driveMotor.getVelocity()
+        self.driveMotorPosition = self.driveMotor.getPosition()
+        self.driveMotorVoltage = self.driveMotor.getAppliedOutput()*self.driveMotor.getBusVoltage()
+
+        self.table.putNumber("DriveMotorFL Speed", self.driveMotorSpeedFeedback)
+        self.table.putNumber("DriveMotorFL Position", self.driveMotorPosition)
+        self.table.putNumber("DriveMotorFL percentVoltage", self.driveMotorPercentVoltage)
+        self.table.putNumber("DriveMotorFL Voltage", self.driveMotorVoltage)
+            
+        
 
 
 
