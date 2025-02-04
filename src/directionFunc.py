a331312b73932d1db45f4807f55a4e3b439f2fcf
CallmeElodie <lfam5718@gmail.com>
2025-02-04 15:28:43 -0600

    fixed some logic and changes var names

--- src/directionFunc.py
+++ src/directionFunc.py
@@ -1,37 +1,54 @@
 import math
-pi = 1 #math.pi
-def func(b, a):
-    print("current motor pos: " +str(b))
-    print("current wheel pos: " + str(b/25))
-    print("wheel target pos on joystick: " + str(a))
-    c = b%(2*pi)
-    print("wheel pos after mod 2pi: " + str(c))
-    if c> pi:
-        c -= 2*pi
-    elif c<-pi:
-        c += 2*pi
-    print("wheel pos unwrapped: " + str(c))
-    delta = a-c
-    print("distance between motor pos and target pos: " + str(delta))
-    if delta> pi:
-        delta -= 2*pi
-    elif delta<-pi:
-        delta += 2*pi
-    print("distance between motor pos and target pos (closest distance): " + str(delta))
 
-    print("new wheel position: " + str(b + delta))
-    print("full wheel rotation: " + str(delta))
-    print("full motor: " + str(delta*25))
-    return (b+delta)*25
+# targetPos, motorPos, distance, and unWrappedPos are subject to name change
+
+pi = 1 #math.pi #pi is 1 for testing radians
+gearRatio = 25 # Change to actual gear ratio
+def func(motorPos, targetPos):
+
+    print("current motor pos: " +str(motorPos))
+    print("current wheel pos: " + str(motorPos/gearRatio))
+    print("wheel target pos on joystick: " + str(targetPos))
+
+    unWrappedPos = motorPos%(2*pi)
+    
+    print("motor pos after mod 2pi: " + str(unWrappedPos))
+    
+    if unWrappedPos> pi:
+        unWrappedPos -= 2*pi
+    elif unWrappedPos<-pi:
+        unWrappedPos += 2*pi
+    
+    print("motor pos unwrapped: " + str(unWrappedPos))
+    
+    distance = targetPos-unWrappedPos
+    
+    print("distance between motor pos and target pos: " + str(distance))
+    
+    if distance> pi:
+        distance -= 2*pi
+    elif distance<-pi:
+        distance += 2*pi
+    
+    print("distance between motor pos and target pos (closest distance): " + str(distance))
+
+    print("new wheel position: " + str(motorPos/gearRatio + distance))
+    print("full wheel rotation: " + str(distance))
+    print("full motor: " + str(distance*gearRatio))
+    
+    return (motorPos+(distance*gearRatio))
     
 
-listB = [99.25*pi, -99.25*pi, 0.25*pi, -0.25*pi, 0.75*pi, -0.75*25]
-listA = [-0.5*pi, 0.5*pi, -0.75*pi, 0.75*pi]
+#For testing:
 
-for i in range(len(listB)*len(listA)):
-    a = i%4
-    y = i%6
-    print("motor new position: " + str(func(listB[y], listA[a])))
-    print("finis")
+#Test variables
+# listB = [99.25*pi, -99.25*pi, 0.25*pi, -0.25*pi, 0.75*pi, -0.75*25]
+# listA = [-0.5*pi, 0.5*pi, -0.75*pi, 0.75*pi]
+
+# for i in range(len(listB)*len(listA)):
+#     targetPos = i%len(listA)
+#     y = i%len(listB)
+#     print("motor new position: " + str(func(listB[y], listA[targetPos])))
+#     print("finis")
 
     
