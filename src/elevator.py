ea9b75be2a5e7f432ac9e6f2b2deb1afb481a82b
mushroom_0291 <meboblikeyoutoo@gmail.com>
2025-02-04 19:07:13 -0600

    I locked in

--- src/elevator.py
+++ src/elevator.py
@@ -1,5 +1,6 @@
 from robotHAL import RobotHALBuffer
 from ntcore import NetworkTableInstance
+from wpilib import Timer
 
 
 class ElevatorSubsystem:
@@ -8,8 +9,17 @@ class ElevatorSubsystem:
         self.table.putNumber("Elevator setpoint offset", 0)
         self.table.putNumber("Elevator arbFF offset", 0)
         self.setpoint = 3
+        self.speed = 0
+        self.maxSpeed = 1
+        self.previousTime = Timer.getFPGATimestamp()
+        self.repeats = 0
 
     def update(self, hal: RobotHALBuffer, up: float, down: float):
+        self.currentTime = Timer.getFPGATimestamp()
+        self.elapsed = self.currentTime - self.previousTime
+        self.previousTime = self.currentTime
+        self.repeats = 0.2 / self.elapsed
+
         self.table.putNumber("Elevator up", up)
         self.table.putNumber("Elevator down", down)
         if up < 0.1:
@@ -17,7 +27,34 @@ class ElevatorSubsystem:
         if down < 0.1:
             down = 0
 
-        self.setpoint += 0.5 * up + (-0.5 * down)
+        if self.speed < self.maxSpeed:
+            if up > 0.1:
+                if (
+                    self.maxSpeed
+                    >= self.speed + ((up * 0.1) * self.maxSpeed) / self.repeats
+                ):
+                    self.speed += ((up / 10) * self.maxSpeed) / self.repeats
+                else:
+                    self.speed = self.maxSpeed
+        elif self.speed > self.maxSpeed * -1:
+            if down > 0.1:
+                if (
+                    self.maxSpeed * -1
+                    >= self.speed - ((down / 10) * self.maxSpeed) / self.repeats
+                ):
+                    self.speed -= ((down / 10) * self.maxSpeed) / self.repeats
+                else:
+                    self.speed = self.maxSpeed * -1
+        elif self.speed >= 0 + (self.maxSpeed / 10) / self.repeats:
+            self.speed -= (self.maxSpeed / 10) / self.repeats
+        elif self.speed <= 0 - (self.maxSpeed / 10) / self.repeats:
+            self.speed += (self.maxSpeed / 10) / self.repeats
+        else:
+            self.speed = 0
+
+        # self.setpoint += 0.5 * self.up + (-0.5 * self.down)
+        self.setpoint += self.speed
+
         if self.setpoint < 0:
             self.setpoint = 0
         if self.setpoint > 50:
