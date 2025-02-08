69446619d727e681131d262d228a94c7266e60e4
Magikarp4536 <sbrooker001@stpaul.k12.mn.us>
2025-02-08 10:28:02 -0600

    made code "work" with prox sensors, added manual

--- src/manipulator.py
+++ src/manipulator.py
@@ -9,6 +9,7 @@ class ManipulatorSubsystem:
     INTAKE = 1
     STORED = 2
     SHOOT = 3
+    MANUAL = 4
 
     def __init__(self):
         
@@ -16,21 +17,25 @@ class ManipulatorSubsystem:
         self.table = NetworkTableInstance.getDefault().getTable("telemetry")
         
 
-    def update(self, buf:RobotHALBuffer, YButton:bool):
+    def update(self, buf:RobotHALBuffer, YButton:bool, pov:float):
         
         if self.state == self.IDLE:
             buf.manipulatorVolts = 0
             if buf.manipulatorSensorReverse:
                 self.state = self.INTAKE
+            if pov == 180:
+                self.state = self.MANUAL
 
         elif self.state == self.INTAKE:
             buf.manipulatorVolts = 1
 
-            if buf.manipulatorSensorForward and not buf.manipulatorSensorReverse:
+            if not buf.manipulatorSensorForward and not buf.manipulatorSensorReverse:
                 self.state = self.IDLE
 
-            if not buf.manipulatorSensorForward:
+            if buf.manipulatorSensorForward and not buf.manipulatorSensorReverse:
                 self.state = self.STORED
+            if pov == 180:
+                self.state = self.MANUAL
                 
                 
 
@@ -40,11 +45,24 @@ class ManipulatorSubsystem:
                 self.state = self.SHOOT
 
             self.startTime = wpilib.getTime()
+            if pov == 180:
+                self.state = self.MANUAL
             
         elif self.state == self.SHOOT:
             
-            buf.manipulatorVolts = 1.75
-            if wpilib.getTime() - self.startTime > 2.6:
+            buf.manipulatorVolts = 5
+            if wpilib.getTime() - self.startTime > 1.5:
                 self.state = self.IDLE
+            if pov == 180:
+                self.state = self.MANUAL
+
+        elif self.state == self.MANUAL:
+            if YButton:
+                buf.manipulatorVolts = 5
+            else:
+                buf.manipulatorVolts = 0
+            if pov == 0:
+                self.state = self.IDLE
+            
 
         self.table.putNumber("state", self.state) 
