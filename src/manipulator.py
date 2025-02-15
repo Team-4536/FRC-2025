35c7eda65b1e541b4a06ff1c0c958a31b1185a5e
Magikarp4536 <sbrooker001@stpaul.k12.mn.us>
2025-02-15 13:18:06 -0600

    added rudimentary arm control

--- src/manipulator.py
+++ src/manipulator.py
@@ -13,13 +13,26 @@ class ManipulatorSubsystem:
         STORED = 2
         SHOOT = 3
         MANUAL = -1
+        GOINGUP = 4
+        UP = 5
+        GOINGDOWN = 6
+
+    debug = True
 
     def __init__(self):
 
         self.state = self.ManipulatorState.IDLE
         self.table = NetworkTableInstance.getDefault().getTable("telemetry")
 
-    def update(self, buf: RobotHALBuffer, AButton: bool, LBumper: bool):
+    def update(
+        self,
+        buf: RobotHALBuffer,
+        AButton: bool,
+        LBumper: bool,
+        BButton: bool,
+        RBumper: bool,
+    ):
+        self.table.putBoolean("manRbumper", RBumper)
 
         if LBumper and not self.state == self.ManipulatorState.MANUAL:
             self.state = self.ManipulatorState.MANUAL
@@ -48,18 +61,53 @@ class ManipulatorSubsystem:
         elif self.state == self.ManipulatorState.SHOOT:
             buf.manipulatorVolts = 8
             if (
-                wpilib.getTime() - self.startTime > 1.5 and not buf.secondManipulatorSensor
+                wpilib.getTime() - self.startTime > 1.5
+                and not buf.secondManipulatorSensor
             ):  # <how long the shooting goes for in sec
                 self.state = self.ManipulatorState.IDLE
 
         elif self.state == self.ManipulatorState.MANUAL:
+            self.table.putNumber("maniphello world", 42)
             if AButton:
                 buf.manipulatorVolts = 8
             else:
                 buf.manipulatorVolts = 0
+            if BButton:
+                buf.armVolts = 1
+            elif RBumper:
+                buf.armVolts = -1
+            else:
+                buf.armVolts = 0
+
+            # turn rbumper into a way to switch between arm modes
             if LBumper:
                 self.state = self.ManipulatorState.IDLE
 
-        
+        # arm things
+        # limit switches normaly open
+        # false until you touch them
 
-        self.table.putString("state", self.state.name)
+        # if self.state == self.ManipulatorState.IDLE:
+        # buf.armVolts = 0
+        # if BButton:
+        # self.state = self.ManipulatorState.GOINGUP
+
+        # elif self.state == self.ManipulatorState.GOINGUP:
+        # buf.armVolts = 5
+        # if buf.frontArmLimitSwitch:
+        # self.state = self.ManipulatorState.UP
+
+        # elif self.state == self.ManipulatorState.UP:
+        # buf.armVolts = 0
+        # if BButton:
+        # self.state = self.ManipulatorState.GOINGDOWN
+
+        # elif self.state == self.ManipulatorState.GOINGDOWN:
+        # buf.armVolts = -5
+        # if buf.frontArmLimitSwitch:
+        # self.state = self.ManipulatorState.IDLE
+
+        if self.debug == True:
+            self.table.putString("maniState", self.state.name)
+            self.table.putNumber("manipulator voltage", buf.manipulatorVolts)
+            self.table.putNumber("maniArm voltage", buf.armVolts)
--- src/simHAL.py
+++ src/simHAL.py
@@ -7,7 +7,7 @@ from robotHAL import RobotHALBuffer
 from swerveDrive import SwerveDrive
 from timing import TimeData
 from wpimath.geometry import Rotation2d, Translation2d
-
+from manipulator import ManipulatorSubsystem
 
 class RobotSimHAL:
     def __init__(self):
@@ -15,6 +15,8 @@ class RobotSimHAL:
         self.table = NetworkTableInstance.getDefault().getTable("telemetry")
         self.table.putBoolean("first manipulator sensor", False)
         self.table.putBoolean("second manipulator sensor", False)
+        self.table.putNumber("arm voltage", 0)
+        self.table.putNumber("manipulator voltage", 0)
 
     def update(self, buf: RobotHALBuffer, time: TimeData) -> None:
         buf.secondManipulatorSensor = self.table.getBoolean(
@@ -22,6 +24,12 @@ class RobotSimHAL:
         )
         buf.firstManipulatorSensor = self.table.getBoolean(
             "first manipulator sensor", False
+        )
+        buf.armVolts = self.table.getNumber(
+            "arm voltage", 0
+        )
+        buf.manipulatorVolts = self.table.getNumber(
+            "manipulator voltage", 0
         )
 
     def resetGyroToAngle(self, ang: float) -> None:
