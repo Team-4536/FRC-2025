6e37c030cb4d1666568f3cf46e09970c163544c2
Jack Nemmers <jtn0426@gmail.com>
2025-03-08 12:18:45 -0600

    Blank Doc

--- src/robot.py
+++ src/robot.py
@@ -17,99 +17,25 @@ from IntakeChute import IntakeChute
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
-
-        self.time = TimeData(None)
-        self.hal = robotHAL.RobotHALBuffer()
-        self.hardware: robotHAL.RobotHAL | RobotSimHAL
-        if self.isSimulation():
-            self.hardware = RobotSimHAL()
-        else:
-            self.hardware = robotHAL.RobotHAL()
-
-        self.hardware.update(self.hal, self.time)
-
-        self.table = NetworkTableInstance.getDefault().getTable("telemetry")
-
-        self.driveCtrlr = wpilib.XboxController(0)
-        self.mechCtrlr = wpilib.XboxController(1)
-        self.buttonPanel = wpilib.Joystick(4)
-
-        self.swerveDrive: SwerveDrive = SwerveDrive()
-        self.elevatorSubsystem = ElevatorSubsystem()
-        self.manipulatorSubsystem = ManipulatorSubsystem()
-        self.intakeChute = IntakeChute()
+        pass
 
     def robotPeriodic(self) -> None:
-        self.time = TimeData(self.time)
-        self.hal.publish(self.table)
-        self.hal.stopMotors()
+        pass
 
     def teleopInit(self) -> None:
         pass
 
     def teleopPeriodic(self) -> None:
-        self.hal.stopMotors()  # Keep this at the top of teleopPeriodic
-
-        self.swerveDrive.update(
-            self.hal,
-            self.driveCtrlr.getLeftX(),
-            -self.driveCtrlr.getLeftY(),
-            self.driveCtrlr.getRightX(),
-            self.driveCtrlr.getRightTriggerAxis(),
-        )
-
-        self.elevatorSubsystem.update(
-            self.hal,
-            self.mechCtrlr.getRightTriggerAxis(),
-            self.mechCtrlr.getLeftTriggerAxis(),
-            self.mechCtrlr.getYButtonPressed(),
-            self.mechCtrlr.getPOV(),
-            self.mechCtrlr.getXButton(),
-            self.mechCtrlr.getBButton(),
-        )
-
-        self.intakeChute.update(
-            self.hal,
-            self.driveCtrlr.getLeftBumper(),
-            self.driveCtrlr.getRightBumper(),
-            self.driveCtrlr.getBButtonPressed(),
-            self.driveCtrlr.getYButtonPressed(),
-        )
-
-        self.manipulatorSubsystem.update(
-            self.hal,
-            self.mechCtrlr.getAButton(),
-            self.mechCtrlr.getLeftBumperPressed(),
-        )
-
-        if self.driveCtrlr.getStartButton():
-            self.hardware.resetGyroToAngle(0)
-
-        # Keep the lines below at the bottom of teleopPeriodic
-        self.hal.publish(self.table)
-        self.hardware.update(self.hal, self.time)
+        pass
 
     def autonomousPeriodic(self) -> None:
-        self.hal.stopMotors()  # Keep this at the top of autonomousPeriodic
-
-        self.intakeChute.update(
-            self.hal,
-            False,
-            False,
-            False,
-            False,
-        )
-
-        self.hardware.update(
-            self.hal, self.time
-        )  # Keep this at the bottom of autonomousPeriodic
+        pass
 
     def disabledInit(self) -> None:
-        self.disabledPeriodic()
+        pass
 
     def disabledPeriodic(self) -> None:
-        self.hal.stopMotors()
-        self.hardware.update(self.hal, self.time)
+        pass
 
 
 if __name__ == "__main__":
