d55486e2d6366b97c3830bfc0b9cfb8d6d7f40fc
Mark Christenson <mark@christensons.net>
2025-03-12 17:01:59 -0500

    add error checking

--- src/LED.py
+++ src/LED.py
@@ -6,13 +6,20 @@ import math
 class LEDSignals:
     def __init__(self, deviceId: int = 0):
         self.can = CAN(deviceId)
-        self.LEDcounter = 0
+        self.period = 5
+        self.counter = 0
 
     def update(self, manipulatorState: int, elevatorPos: float):
-        self.LEDcounter += 1
-        if self.LEDcounter >= 10:
-            byte_array = bytearray(2)
-            byte_array[0] = math.floor(elevatorPos * (20 / 9))
-            byte_array[1] = manipulatorState
-            self.can.writePacket(byte_array, 0)
-            self.LEDcounter = 0
+        if self.counter <= 0:
+            try:
+                byte_array = bytearray(2)
+                byte_array[0] = max(math.floor(elevatorPos * (20 / 9)), 0)
+                byte_array[1] = manipulatorState
+                self.can.writePacket(byte_array, 0)
+                self.LEDcounter = 0
+            except e:
+                print(e)
+
+            self.counter = self.period
+        else:
+            self.counter = self.counter - 1
