69ff4e9c0663126162b46349dadcc0399abe0c4c
connormaass <connormaass2@gmail.com>
2025-09-11 16:24:17 -0500

    oh yeH

--- src/robot.py
+++ src/robot.py
@@ -5,19 +5,19 @@ from rev import SparkMax
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
-
-        self.time = TimeData(None)
-
         self.driveCtrlr = wpilib.XboxController(0)
 
-        self.Motor =  SparkMax(11, rev.SparkMax.MotorType.kBrushless)
+        self.Motor =  SparkMax(2, rev.SparkMax.MotorType.kBrushless)
         self.Motor.set(0)
         
     def robotPeriodic(self) -> None:
-        self.time = TimeData(self.time)
+       pass
         
     def teleopInit(self) -> None:
         self.Motor.set(0.2)
+
+    def teleopPeriodic(self):
+        pass
        
 
 if __name__ == "__main__":
