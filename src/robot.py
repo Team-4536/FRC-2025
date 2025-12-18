26bce6736c12975eef8e011eb90f226e86e024a7
connormaass <connormaass2@gmail.com>
2025-12-18 16:30:50 -0600

    deploy's with no errors

--- src/robot.py
+++ src/robot.py
@@ -8,15 +8,20 @@ from wpimath.kinematics import ChassisSpeeds, SwerveModulePosition
 from robotHAL import RobotHAL, RobotHALBuffer
 from swerveDrive import SwerveDrive
 from wpimath.units import radians
+import rev
+from time import sleep
 
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
-
+        # turnMotorFL = rev.SparkMax(2, rev.SparkMax.MotorType.kBrushless)
         self.hal = robotHAL.RobotHALBuffer()
+
+        sleep(1)
+
         self.hardware = robotHAL.RobotHAL()
 
-        self.hardware.update(self.hal)
+        # self.hardware.update(self.hal)
 
         self.table = NetworkTableInstance.getDefault().getTable("telemetry")
 
@@ -27,17 +32,18 @@ class Robot(wpilib.TimedRobot):
 
     def robotPeriodic(self) -> None:
 
-        self.hal.publish(self.table)
-        self.hal.stopMotors()
+        # self.hal.publish(self.table)
+        # self.hal.stopMotors()
+        pass
 
     def teleopInit(self) -> None:
         self.setpointActiveLeft = False
         self.setpointActiveRight = False
 
-        self.hal.rotPIDToggle = False
+        # self.hal.rotPIDToggle = False
 
     def teleopPeriodic(self) -> None:
-        self.hal.stopMotors()  # Keep this at the top of teleopPeriodic
+        # self.hal.stopMotors()  # Keep this at the top of teleopPeriodic
 
         if not self.setpointActiveLeft and not self.setpointActiveRight:
             self.swerveDrive.update(
@@ -61,25 +67,25 @@ class Robot(wpilib.TimedRobot):
 
         # convert POV buttons to bool values (sorry michael this code may be hard to look at)
 
-        if self.driveCtrlr.getStartButton():
-            self.hardware.resetGyroToAngle(0)
+        # if self.driveCtrlr.getStartButton():
+        #     self.hardware.resetGyroToAngle(0)
 
         # abs drive toggle
-        if self.driveCtrlr.getLeftStickButtonPressed():
-            self.hal.fieldOriented = not self.hal.fieldOriented
+        # if self.driveCtrlr.getLeftStickButtonPressed():
+        #     self.hal.fieldOriented = not self.hal.fieldOriented
 
-        if self.driveCtrlr.getYButtonPressed():
-            self.hal.rotPIDsetpoint = 240
-            self.hal.rotPIDToggle = True
-        elif self.driveCtrlr.getXButtonPressed():
-            self.hal.rotPIDsetpoint = 300
-            self.hal.rotPIDToggle = True
-        elif self.driveCtrlr.getAButtonPressed():
-            self.hal.rotPIDsetpoint = 60
-            self.hal.rotPIDToggle = True
-        elif self.driveCtrlr.getBButtonPressed():
-            self.hal.rotPIDsetpoint = 120
-            self.hal.rotPIDToggle = True
+        # if self.driveCtrlr.getYButtonPressed():
+        #     self.hal.rotPIDsetpoint = 240
+        #     self.hal.rotPIDToggle = True
+        # elif self.driveCtrlr.getXButtonPressed():
+        #     self.hal.rotPIDsetpoint = 300
+        #     self.hal.rotPIDToggle = True
+        # elif self.driveCtrlr.getAButtonPressed():
+        #     self.hal.rotPIDsetpoint = 60
+        #     self.hal.rotPIDToggle = True
+        # elif self.driveCtrlr.getBButtonPressed():
+        #     self.hal.rotPIDsetpoint = 120
+        #     self.hal.rotPIDToggle = True
 
     def autonomousInit(self) -> None:
         pass
@@ -91,9 +97,9 @@ class Robot(wpilib.TimedRobot):
         self.disabledPeriodic()
 
     def disabledPeriodic(self) -> None:
-        self.hal.stopMotors()
-        self.hardware.update(self.hal)
+        # self.hal.stopMotors()
+        # self.hardware.update(self.hal)
+        pass
 
-
-if __name__ == "__main__":
-    wpilib.run(Robot)
+    def testInit(self):
+        self.turnMotorFL = rev.SparkMax(2, rev.SparkMax.MotorType.kBrushless)
